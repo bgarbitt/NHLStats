a76e4f9ded12ae099037a69e7d29540998fa0ced
bgarbitt <bgarbitt@ualberta.ca>
2019-04-06 00:54:38 -0600

    Set up an API on AWS that returns urls for each team's logo.

--- src/App.js
+++ src/App.js
@@ -4,27 +4,37 @@ import './App.css';
 
 import Team from './Team';
 
-const teams = [{
-      "id": 1,
-      "name": "New Jersey Devils",
-  }, {
-      "id": 2,
-      "name": "Edmonton Oilers",
-  }, {
-      "id": 3,
-      "name": "Calgary Flames",
-  },
-];
-
-
 class App extends Component {
+  state = { teams: [], teamMediaLinks: {} }
+
+  async componentDidMount() {
+    try {
+      const nhlApiTeamUrl = 'https://statsapi.web.nhl.com/api/v1/teams';
+      const awsApiLogosUrl = 'https://4bd4fqbdl1.execute-api.us-east-1.amazonaws.com/demo-1/logos';
+
+      const apiCallResults = await fetch(nhlApiTeamUrl);
+      const teams = await (await apiCallResults.json()).teams;
+
+      const awsCallResults = await fetch(awsApiLogosUrl);
+      const teamMediaLinks = await awsCallResults.json();
+
+      this.setState({ teams, teamMediaLinks });
+    } catch (e) {
+      console.log(e); // eslint-disable-line
+    }
+  }
+
   render() {
+    const { teams, teamMediaLinks } = this.state;
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
         </header>
-        {teams.map(team => <Team key={team.id} team={team} />)}
+        {teams.map((team) => {
+          const logoUrl = teamMediaLinks[team.name]['team-logo'];
+          return <Team key={team.id} team={team} logoUrl={logoUrl} />;
+        })}
       </div>
     );
   }
--- src/Team.js
+++ src/Team.js
@@ -1,14 +1,37 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-class Team extends Component {
-  render() {
-    const { team } = this.props;
-    return (
-      <div>
-        {team.name}
-      </div>
-    );
-  }
-}
+// Components
+const Team = ({ team, logoUrl }) => (
+  <TeamInfo name={team.name} location={team.locationName} logoUrl={logoUrl} />
+);
+
+const TeamInfo = ({ name, location, logoUrl }) => (
+  <div>
+    <h3>{ name }</h3>
+    <p>{ location }</p>
+    <img src={logoUrl} alt="team-logo" />
+
+  </div>
+);
 
 export default Team;
+
+// Prop Types
+Team.propTypes = {
+  team: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    location: PropTypes.string,
+  }).isRequired,
+  logoUrl: PropTypes.string,
+};
+
+TeamInfo.propTypes = {
+  name: PropTypes.string.isRequired,
+  location: PropTypes.string,
+  logoUrl: PropTypes.string,
+};
+
+// Default Props
+Team.defaultProps = { logoUrl: 'notFound.png' };
+TeamInfo.defaultProps = { location: 'Nowhere', logoUrl: 'notFound.png' };
